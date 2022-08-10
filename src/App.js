d9b84b3cad30c5511c518ab6ef829e771162d0c1
Sofia Makita <smakita@icebergsolutions.com>
2022-08-10 16:00:49 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+import { Routes, Route, BrowserRouter as Router }from 'react-router-dom';
+// Pages
+import 
+
+function App() {
+  return (
+    <div>
+      <Router className="App">
+      Header 
+
+      <br/><br/><br/><br/>
+      
+      <Routes>
+        <Route path='' element=''/>
+      </Routes>
+
+      Footer
+      </Router>
+    </div>
+  );
+}
+
+export default App;
